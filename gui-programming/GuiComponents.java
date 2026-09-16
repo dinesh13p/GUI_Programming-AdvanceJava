a1e520ef62a41f9f1968665a78ce6603a30d4a9c
Dinesh Poudel <dineshp4297501@gmail.com>
2026-09-16 19:47:01 +0545

    Added Label, Text Area, Password Field, Text Field, Slider

--- gui-programming/GuiComponents.java
+++ gui-programming/GuiComponents.java
@@ -1,5 +1,10 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class GuiComponents {
     public static void main(String[] args) {
@@ -19,6 +24,8 @@ public class GuiComponents {
         //Set Layout
         jFrame.setLayout(null);
 
+        //JButton
+
         JButton firstButton = new JButton("First Button");
         firstButton.setBounds(100, 200, 150, 80);
         jFrame.add(firstButton);
@@ -32,10 +39,39 @@ public class GuiComponents {
         jFrame.add(thirdButton);
 
         //JLabel
+
+        JLabel firstLabel = new JLabel("this is label");
+        firstLabel.setBounds(300, 200, 200, 30);
+        jFrame.add(firstLabel);
+
         //JTextArea
-        //JPassowrdField
+
+        JTextArea firstTextArea = new JTextArea("Type here...");
+        firstTextArea.setLineWrap(true);
+        firstTextArea.setWrapStyleWord(true);
+        firstTextArea.setBounds(300, 250, 200, 80);
+        jFrame.add(firstTextArea);
+
+        //JPasswordField
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(300, 350, 200, 30);
+        jFrame.add(passwordField);
+
         //JTextField
+
+        JTextField textField = new JTextField();
+        textField.setBounds(300, 400, 200, 30);
+        jFrame.add(textField);
+
         //JSlider
+
+        JSlider slider = new JSlider(0, 100, 50);
+        slider.setMajorTickSpacing(25);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setBounds(300, 450, 250, 60);
+        jFrame.add(slider);
 
 
         // Set Visibility
