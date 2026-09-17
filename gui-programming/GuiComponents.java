a22a647c6802d3b008d21d555931872c1092979b
Dinesh Poudel <dineshp4297501@gmail.com>
2026-09-17 13:07:12 +0545

    GUI Components

--- gui-programming/GuiComponents.java
+++ gui-programming/GuiComponents.java
@@ -1,4 +1,5 @@
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
@@ -12,7 +13,7 @@ public class GuiComponents {
         JFrame jFrame = new JFrame("GUI Components");
 
         // 2. Set Default size
-        jFrame.setSize(800, 900);
+        jFrame.setSize(1000, 1200);
 
         // 3. Close Operation
         // jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //termination
@@ -21,10 +22,11 @@ public class GuiComponents {
 
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit -> running
 
-        //Set Layout
+        // 3. Set Layout
         jFrame.setLayout(null);
 
-        //JButton
+        // 4. Add Button Component
+        // JButton
 
         JButton firstButton = new JButton("First Button");
         firstButton.setBounds(100, 200, 150, 80);
@@ -38,41 +40,58 @@ public class GuiComponents {
         thirdButton.setBounds(100, 400, 150, 80);
         jFrame.add(thirdButton);
 
-        //JLabel
+        // 5. Create Checkbox
+        JLabel checkBoxJLabel = new JLabel("Hobbies:");
+        checkBoxJLabel.setBounds(400, 300, 150, 80);
+        JCheckBox cricket = new JCheckBox("Cricket");
+        cricket.setBounds(400, 400, 150, 80);
+        JCheckBox volleyball = new JCheckBox("volleyball");
+        volleyball.setBounds(500, 400, 150, 80);
+        JCheckBox football = new JCheckBox("football");
+        football.setBounds(600, 400, 150, 80);
+        JCheckBox basketball = new JCheckBox("basketball");
+        basketball.setBounds(700, 400, 150, 80);
 
-        JLabel firstLabel = new JLabel("this is label");
-        firstLabel.setBounds(300, 200, 200, 30);
-        jFrame.add(firstLabel);
+        jFrame.add(checkBoxJLabel);
+        jFrame.add(cricket);
+        jFrame.add(volleyball);
+        jFrame.add(football);
+        jFrame.add(basketball);
 
-        //JTextArea
+        // // JLabel
 
-        JTextArea firstTextArea = new JTextArea("Type here...");
-        firstTextArea.setLineWrap(true);
-        firstTextArea.setWrapStyleWord(true);
-        firstTextArea.setBounds(300, 250, 200, 80);
-        jFrame.add(firstTextArea);
+        // JLabel firstLabel = new JLabel("this is label");
+        // firstLabel.setBounds(300, 200, 200, 30);
+        // jFrame.add(firstLabel);
 
-        //JPasswordField
+        // // JTextArea
 
-        JPasswordField passwordField = new JPasswordField();
-        passwordField.setBounds(300, 350, 200, 30);
-        jFrame.add(passwordField);
+        // JTextArea firstTextArea = new JTextArea("Type here...");
+        // firstTextArea.setLineWrap(true);
+        // firstTextArea.setWrapStyleWord(true);
+        // firstTextArea.setBounds(300, 250, 200, 80);
+        // jFrame.add(firstTextArea);
 
-        //JTextField
+        // // JPasswordField
 
-        JTextField textField = new JTextField();
-        textField.setBounds(300, 400, 200, 30);
-        jFrame.add(textField);
+        // JPasswordField passwordField = new JPasswordField();
+        // passwordField.setBounds(300, 350, 200, 30);
+        // jFrame.add(passwordField);
 
-        //JSlider
+        // // JTextField
 
-        JSlider slider = new JSlider(0, 100, 50);
-        slider.setMajorTickSpacing(25);
-        slider.setPaintTicks(true);
-        slider.setPaintLabels(true);
-        slider.setBounds(300, 450, 250, 60);
-        jFrame.add(slider);
+        // JTextField textField = new JTextField();
+        // textField.setBounds(300, 400, 200, 30);
+        // jFrame.add(textField);
 
+        // // JSlider
+
+        // // JSlider slider = new JSlider(0, 100, 50);
+        // // slider.setMajorTickSpacing(25);
+        // // slider.setPaintTicks(true);
+        // // slider.setPaintLabels(true);
+        // // slider.setBounds(300, 450, 250, 60);
+        // // jFrame.add(slider);
 
         // Set Visibility
         jFrame.setVisible(true);
